646e5a504dcb49abc76a4f52964bb51b3efaa352
Rohit <rohitkumar10701@gmail.com>
2024-11-30 15:56:02 +0530

    python : added script to get and convert string to image

---
+++ python/extract_pixels_to_string.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('./image.png')
+
+height, width, channels = image.shape
+
+flattened = image.flatten()
+pixel_string = ''.join([f'{pixel:03}' for pixel in flattened])  
+metadata = f"{height},{width},{channels}"
+
+full_string = metadata + "|" + pixel_string
+
+with open('pixels_with_metadata.txt', 'w') as f:
+    f.write(full_string)
+
+print("Pixel string with metadata saved to 'pixels_with_metadata.txt'.")
---
+++ python/revert_string_to_image.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+with open('pixels_with_metadata.txt', 'r') as f:
+    full_string = f.read()
+
+metadata, pixel_string = full_string.split('|')
+
+height, width, channels = map(int, metadata.split(','))
+
+pixels = np.array([int(pixel_string[i:i+3]) for i in range(0, len(pixel_string), 3)])
+
+pixels = pixels.reshape((height, width, channels))
+
+pixels = np.clip(pixels, 0, 255).astype(np.uint8)
+
+cv2.imwrite('reverted_image.jpg', pixels)
+
+cv2.imshow('Reverted Image', pixels)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print("Reverted image saved as 'reverted_image.jpg'.")
